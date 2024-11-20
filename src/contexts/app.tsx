91be872b42c64f3bc6a6a093b79f4462ef6a8457
alphadev2018 <freshwebcoder@hotmail.com>
2024-11-20 10:46:13 +0800

    add appContext

---
+++ src/contexts/app.tsx
@@ -0,0 +1,69 @@
+import { h, createContext, FunctionComponent } from 'preact';
+import { useContext } from 'preact/hooks';
+import { signal, useSignal } from '@preact/signals';
+
+interface AppContextType {
+  color: ReturnType<typeof useSignal<string>>;
+  zoom: ReturnType<typeof useSignal<number>>;
+  rotationX: ReturnType<typeof useSignal<number>>;
+  rotationY: ReturnType<typeof useSignal<number>>;
+  setColor: (val: string) => void;
+  setZoom: (val: number) => void;
+  setRotationX: (val: number) => void;
+  setRotationY: (val: number) => void;
+}
+
+const AppContext = createContext<AppContextType>({
+  color: signal('#ffffff'),
+  zoom: signal(2),
+  rotationX: signal(0),
+  rotationY: signal(0),
+  setColor: () => {},
+  setZoom: () => {},
+  setRotationX: () => {},
+  setRotationY: () => {},
+});
+
+export const useApp = () => {
+  return useContext(AppContext);
+};
+
+export const AppProvider: FunctionComponent = ({ children }) => {
+  const color = useSignal('#ffffff');
+  const zoom = useSignal(2);
+  const rotationX = useSignal(0);
+  const rotationY = useSignal(0);
+
+  const setColor = (val: string) => {
+    color.value = val;
+  };
+
+  const setZoom = (val: number) => {
+    zoom.value = val;
+  };
+
+  const setRotationX = (val: number) => {
+    rotationX.value = val;
+  };
+
+  const setRotationY = (val: number) => {
+    rotationY.value = val;
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        color,
+        zoom,
+        rotationX,
+        rotationY,
+        setColor,
+        setZoom,
+        setRotationX,
+        setRotationY,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
